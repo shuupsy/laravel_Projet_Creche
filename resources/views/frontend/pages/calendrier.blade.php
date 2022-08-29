285a0464320568929f78892b55cd04f290da0edb
Mounzz <senmounia@hotmail.com>
2022-08-29 16:27:32 +0200

    lien change

--- resources/views/frontend/pages/calendrier.blade.php
+++ resources/views/frontend/pages/calendrier.blade.php
@@ -6,11 +6,11 @@
 <div class="drop-shadow-md ml-64 bg-white w-72 mt-16 rounded-lg">
     <div class="flex gap-4 p-4 place-content-around">
         <div>
-            <img class="w-12" src="https://www.svgrepo.com/show/38147/moon.svg" alt="">
+            <img class="w-12" src="https://www.svgrepo.com/show/108654/mercury.svg" alt="">
         </div>
         <div class="flex flex-col">
             <p>{{$data[2]->day}}</p>
-            <p>date</p>
+            <p>{{$data[2]->dateDay}} septembre</p>
         </div>
     </div>
 
