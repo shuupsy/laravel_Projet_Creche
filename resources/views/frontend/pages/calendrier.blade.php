f0272caf0928e2544c2241682b7033e1da72ff7b
Mounzz <senmounia@hotmail.com>
2022-08-29 16:08:43 +0200

    debut calendrier

--- resources/views/frontend/pages/calendrier.blade.php
+++ resources/views/frontend/pages/calendrier.blade.php
@@ -3,6 +3,17 @@
 
 @section('content')
 
+<div class="drop-shadow-md ml-64 bg-white w-72 mt-16 rounded-lg">
+    <div class="flex gap-4 p-4 place-content-around">
+        <div>
+            <img class="w-12" src="https://www.svgrepo.com/show/38147/moon.svg" alt="">
+        </div>
+        <div class="flex flex-col">
+            <p>jour</p>
+            <p>date</p>
+        </div>
+    </div>
 
+</div>
 
 @endsection
