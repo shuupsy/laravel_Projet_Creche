ef5eb48a1866012c6ad8095f75ee669e195ba27f
Mounzz <senmounia@hotmail.com>
2022-08-30 19:18:24 +0200

    calendrier 8%

--- public/assets/js/calendrier.js
+++ public/assets/js/calendrier.js
@@ -1,9 +1,17 @@
 const btnJour = document.querySelectorAll('#btnJour');
+const container = document.querySelector('.containerCalendrier');
+
+const contenu = document.querySelector('.noShow');
+
 
 
 btnJour.forEach(element => {
     element.addEventListener("click", ()=>{
-        console.log('hello');
+        btnJour.forEach(btn=>{
+            btn.removeAttribute('class', 'activeDay');
+            contenu.removeAttribute('class', 'noShow');
+        });
+        element.setAttribute('class', 'activeDay');
+        contenu.setAttribute('class', 'contenuCalendrier');
     });
-
  });
