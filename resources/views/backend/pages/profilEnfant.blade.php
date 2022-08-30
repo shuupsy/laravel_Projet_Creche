5d072f5d318ad545bd50bbcebac392443490df3e
shuupsy <sylvaine.ruan@gmail.com>
2022-08-30 14:28:37 +0200

    Push avant MAJ Yves Backend

--- resources/views/backend/pages/profilEnfant.blade.php
+++ resources/views/backend/pages/profilEnfant.blade.php
@@ -83,6 +83,42 @@
     </div> {{-- FIN ICONES --}}
 
 
+    <div class="rounded-lg border border-zinc-800 text-center">
+        <button class="modalUn">
+            <img src="/assets/img/clock.svg" alt="svg clock time" width="50px">
+        </button>
+        <p class='text-sm'>Heure</p>
+    </div>
+    <div class="relative flex justify-center">
+        <div class='hidden' id="modalFull">
+            <div class="">
+                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+                <div class="relative inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl dark:bg-gray-900 sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                    <div>
+                        <div class="flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-700 dark:text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                            </svg>
+                        </div>
+
+                        <div class="mt-2 text-center">
+                            <h3 class="text-lg font-medium leading-6 text-gray-800 dark:text-white" id="modal-title">Horaire d'Arrivée / de Sortie</h3>
+                            <button>ARRIVEE (matin)</button>
+                            <button>SORTIE (soir)</button>
+                        </div>
+                    </div>
+
+                    <div class="mt-5 sm:flex sm:items-center sm:justify-between">
+
+                        <div class="sm:flex sm:items-center ">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
 @endsection
