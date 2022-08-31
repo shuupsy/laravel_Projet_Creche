ffc1a8fb2b493ddb0be48736b06386d7e94b664d
Mounzz <senmounia@hotmail.com>
2022-08-31 11:43:39 +0200

    modif home nurse

--- resources/views/backend/pages/home.blade.php
+++ resources/views/backend/pages/home.blade.php
@@ -4,6 +4,8 @@
 
     <div class="w-full flex flex-wrap justify-center gap-6">
         @foreach ($kids as $kid)
+
+            @if ($kid->genre == "masculin")
             <div class="w-64 bg-white drop-shadow-md p-2 my-8 rounded-lg">
 
                 <a class="flex flex-col items-center" href="/kid/{{ $kid -> id }}">
@@ -11,10 +13,27 @@
                         src="https://www.svgrepo.com/show/296592/baby.svg" alt="image description">
 
                     <h1 class="font-extrabold text-4xl">{{ $kid->name }}</h1>
-                    <p>section des petits</p>
+                    <p>section des {{ $kid->section }}</p>
                 </a>
 
             </div>
+            @else
+            <div class="w-64 bg-white drop-shadow-md p-2 my-8 rounded-lg">
+
+                <a class="flex flex-col items-center" href="/kid/{{ $kid -> id }}">
+                    <img class="bg-[#FFD6DD] w-24 h-24 rounded-full p-4 -mt-12"
+                        src="https://www.svgrepo.com/show/296610/baby-girl.svg" alt="image description">
+
+                    <h1 class="font-extrabold text-4xl">{{ $kid->name }}</h1>
+                    <p>section des {{ $kid->section }}</p>
+                </a>
+
+            </div>
+            @endif
+
+
+
+
         @endforeach
     </div>
 @endsection
