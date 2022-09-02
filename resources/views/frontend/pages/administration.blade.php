5dd4204cf2b9e67355edd0a439b737ae09671d3d
shuupsy <sylvaine.ruan@gmail.com>
2022-09-02 14:43:41 +0200

    Administration CSS

--- resources/views/frontend/pages/administration.blade.php
+++ resources/views/frontend/pages/administration.blade.php
@@ -1,110 +1,60 @@
 @extends('frontend.layout.index')
 
 @section('content')
+    <section id="admin" class="flex flex-col gap-5">
 
-<div class="flex flex-col ml-10">
+        <div id="admin-title" class="flex justify-center text-xl">
+            <h1>ADMINISTRATION</h1>
+        </div>
 
-    <div class="flex justify-start">
-        <h1>ADMINISTRATION</h1>
-    </div>
-    <div class="">
-        <button
-            class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 text-xs mt-2">
-            UPLOAD CM
-        </button>
-    </div>
 
-    <div class="flex items-center">
-        <div>
-            <a href="">
-                <div class="flex justify-center items-center h-28 w-28 bg-red-500 rounded-full my-5">
+        <div id="upload-cm">
+            <button
+                class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 text-xs mt-2">
+                Nouveau certificat médical
+            </button>
+        </div>
+
+        <div id="admin-docs" class="flex flex-col gap-5">
+
+            {{-- Contrat --}}
+            <div class='flex items-center gap-5'>
+                <div class="flex justify-center items-center h-20 w-20 border-2 border-neutral-300 text-[#D8E2DC] rounded-full">
                     <i class='bx bxs-file text-5xl'></i>
                 </div>
-            </a>
-        </div>
-        <div class="ml-5">
-            <p>
-                section:{{ $data->section }}
-            </p>
-        </div>
-    </div>
+                <p>Voir le contrat</p>
+            </div>
 
-    <div class="flex items-center">
-        <div>
-            <a href="">
-                <div class="flex justify-center items-center h-28 w-28 bg-red-500 rounded-full my-5">
+
+            {{-- Autorisation de sortie --}}
+            <div class='flex items-center gap-5'>
+                <div class="flex justify-center items-center h-20 w-20 border-2 border-neutral-300 text-[#D8E2DC] rounded-full">
                     <i class='bx bxs-file text-5xl'></i>
                 </div>
-            </a>
-        </div>
-        <div class="ml-5">
-            <p>
-                contrat:{{ $data->contrat }}
-            </p>
-        </div>
-    </div>
+                <p>Autorisation de sortie</p>
+            </div>
 
-    <div class="flex items-center">
-        <div>
-            <a href="">
-                <div class="flex justify-center items-center h-28 w-28 bg-red-500 rounded-full my-5">
-                    <i class='bx bxs-file text-5xl'></i>
+            {{-- Autorisation de photos --}}
+            <div class='flex items-center gap-5'>
+                <div class="flex justify-center items-center h-20 w-20 border-2 border-neutral-300 text-[#D8E2DC] rounded-full">
+                    <i class='bx bxs-camera text-5xl'></i>
                 </div>
-            </a>
-        </div>
-        <div class="ml-5">
-            <p>
-                permission photo:{{ $data->picturePermission }}
-            </p>
-        </div>
-    </div>
+                <p>Autorisation de photos</p>
+            </div>
 
-    <div class="flex items-center">
-        <div>
-            <a href="">
-                <div class="flex justify-center items-center h-28 w-28 bg-red-500 rounded-full my-5">
-                    <i class='bx bxs-file text-5xl'></i>
+            {{-- Jours de présence --}}
+            <div class='flex items-center gap-5'>
+                <div class="flex justify-center items-center h-20 w-20 border-2 border-neutral-300 text-[#D8E2DC] rounded-full">
+                    <i class='bx bx-calendar text-5xl'></i>
                 </div>
-            </a>
-        </div>
-        <div class="ml-5">
-            <p>
-                permission sortie:{{ $data->exitPermission }}
-            </p>
-        </div>
-    </div>
-
-    <div class="flex items-center">
-        <div>
-            <a href="">
-                <div class="flex justify-center items-center h-28 w-28 bg-red-500 rounded-full my-5">
-                    <i class='bx bxs-file text-5xl'></i>
+                <div>
+                    <p>Jours de présence ({{ $data->presence }}) :</p>
+                    <p>{{ $data->dayOfPresence }}</p>
                 </div>
-            </a>
-        </div>
-        <div class="ml-5">
-            <p>
-                nbr jours présence:{{ $data->presence }}
-            </p>
-        </div>
-    </div>
+            </div>
 
-    <div class="flex items-center">
-        <div>
-            <a href="">
-                <div class="flex justify-center items-center h-28 w-28 bg-red-500 rounded-full my-5">
-                    <i class='bx bxs-file text-5xl'></i>
-                </div>
-            </a>
         </div>
-        <div class="ml-5">
-            <p>
-                jour de présence:{{ $data->dayOfPresence }}
-            </p>
-        </div>
-    </div>
 
 
-</div>
-
+    </section>
 @endsection
